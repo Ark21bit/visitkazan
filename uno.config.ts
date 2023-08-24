d6625292b05b1c4721be9bf3c694233c242b2688
Ark21bit <webleif@vk.com>
2023-08-24 20:45:18 +0300

    fix

--- uno.config.ts
+++ uno.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
         presetUno(),
         presetWebFonts({
             fonts: {
-                Inter: 'Inter'
+                Inter: { name: "Inter", weights: [400, 500, 600, 700, 800, 900] }
             },
         }),
         formKitUnocssPlugin()
